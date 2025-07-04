1cc356412a6d496fb5f9ae7e6bfd581e6f2ee586
Ujwal135 <ujjwaldzz3@gmail.com>
2025-07-05 01:00:56 +0530

    introduction-of-java-script

---
+++ Java-script/script.js
@@ -0,0 +1,22 @@
+alert("Hello World..")
+
+console.log("Code is Running .")
+console.log("Code is still running")
+console.log("Code is looking like a wow")
+
+var a = prompt('Enter your number')
+
+var isTrue = confirm("are you sure want live this leave this page and blast your computer")
+
+if(isTrue){
+    console.log('computer is blasting') 
+}
+else{
+    console.log('computer is not blasting ')
+}
+
+console.log('your number is '+ a)
+
+document.title = "Hey This is Ujjwal"
+
+document.body.style.backgroundColor = 'green'
